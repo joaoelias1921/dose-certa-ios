0e6aa8d3c352ba207fac7b9ca66f5e90c37a83c6
João Elias <joao.candias@outlook.com>
2025-12-22 10:27:49 -0300

    Initial Commit

---
+++ dosecerta/dosecertaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dosecertaApp.swift
+//  dosecerta
+//
+//  Created by João Elias Cândido Reis on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct dosecertaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
